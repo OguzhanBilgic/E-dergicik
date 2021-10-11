c9f4db21963105b37e59f9059d5cf0e8bc4310a7
Oğuzhan Bilgiç <oguzhan.bilgic@keove.com>
2021-10-11 11:00:04 +0300

    Initial Commit

---
+++ E-dergicik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-dergicik
+//
+//  Created by Oğuzhan Bilgiç on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
